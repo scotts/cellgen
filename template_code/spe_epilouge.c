67a984e4a3fc047e0a97736afb082da6de23ff3f
jyeom <jyeom>
2008-07-31 03:15:05 +0000

    *** empty log message ***

--- template_code/spe_epilouge.c
+++ template_code/spe_epilouge.c
@@ -1,3 +1,4 @@
+/*
 void compute_bounds(int *start, int *stop, int buff_sz)
 {
 	int total_chunks = (*stop - *start) / buff_sz;
@@ -12,6 +13,42 @@ void compute_bounds(int *start, int *stop, int buff_sz)
 		*start = *start + (SPE_id * thread_chunks * buff_sz);
 		*stop = *start + (thread_chunks * buff_sz);
 	}
+}
+*/
+
+void compute_bounds (int *start, int *stop, int buff_sz)
+{
+  int total_chunks = (*stop - *start) / buff_sz;
+  int thread_chunks = total_chunks / SPE_threads;
+  int rem     = ((*stop - *start) % (buff_sz * SPE_threads)) / buff_sz;
+  int rem_rem = ((*stop - *start) % (buff_sz * SPE_threads)) % buff_sz;
+
+  int id = SPE_threads - rem;
+
+  if (SPE_id == SPE_threads - 1)
+    {
+      if (SPE_id > id)
+          *start = *start + (SPE_id * thread_chunks * buff_sz + buff_sz * (SPE_id - id));
+      else
+          *start = *start + (SPE_id * thread_chunks * buff_sz);
+
+      if (SPE_id >= id)
+          *stop = *start + ((thread_chunks + 1) * buff_sz + rem_rem);
+      else
+          *stop = *start + (thread_chunks * buff_sz + rem_rem);
+    }
+  else
+    {
+      if (SPE_id > id)
+          *start = *start + (SPE_id * thread_chunks * buff_sz + buff_sz * (SPE_id - id));
+      else
+          *start = *start + (SPE_id * thread_chunks * buff_sz);
+
+      if (SPE_id >= id)
+          *stop = *start + ((thread_chunks + 1) * buff_sz);
+      else
+          *stop = *start + (thread_chunks * buff_sz);
+    }
 }
 
 int main()
